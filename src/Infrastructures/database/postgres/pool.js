45caa70d2eec817b49107bbe772ed47be87a6cd1
Restu Edo Setiaji <res90001218@trac.astra.co.id>
2023-11-03 12:53:25 +0700

    Reformat Code

--- src/Infrastructures/database/postgres/pool.js
+++ src/Infrastructures/database/postgres/pool.js
@@ -1,6 +1,6 @@
 /* istanbul ignore file */
 const { Pool } = require('pg');
- 
+
 const testConfig = {
   host: process.env.PGHOST_TEST,
   port: process.env.PGPORT_TEST,
@@ -8,7 +8,7 @@ const testConfig = {
   password: process.env.PGPASSWORD_TEST,
   database: process.env.PGDATABASE_TEST,
 };
- 
+
 const pool = process.env.NODE_ENV === 'test' ? new Pool(testConfig) : new Pool();
- 
+
 module.exports = pool;
--- src/Interfaces/http/api/users/handler.js
+++ src/Interfaces/http/api/users/handler.js
@@ -1,15 +1,16 @@
 const AddUserUseCase = require('../../../../Applications/use_case/AddUserUseCase');
- 
+
 class UsersHandler {
   constructor(container) {
     this._container = container;
- 
+
     this.postUserHandler = this.postUserHandler.bind(this);
   }
- 
+
   async postUserHandler(request, h) {
     const addUserUseCase = this._container.getInstance(AddUserUseCase.name);
     const addedUser = await addUserUseCase.execute(request.payload);
+
     const response = h.response({
       status: 'success',
       data: {
@@ -20,5 +21,5 @@ class UsersHandler {
     return response;
   }
 }
- 
+
 module.exports = UsersHandler;
