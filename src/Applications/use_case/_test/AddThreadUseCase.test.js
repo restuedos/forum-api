66dbb2421589b34fcedb8326f8bb1439e4a25a69
Restu Edo Setiaji <res90001218@trac.astra.co.id>
2023-11-08 11:24:08 +0700

    Add Thread Repository Entity & Use Case

---
+++ src/Applications/use_case/_test/AddThreadUseCase.test.js
@@ -0,0 +1,44 @@
+const AddThread = require('../../../Domains/threads/entities/AddThread');
+const AddedThread = require('../../../Domains/threads/entities/AddedThread');
+const ThreadRepository = require('../../../Domains/threads/ThreadRepository');
+const AddThreadUseCase = require('../AddThreadUseCase');
+
+describe('AddThreadUseCase', () => {
+  it('should orchestrating the add thread action correctly', async () => {
+    // Arrange
+    const useCasePayload = {
+      title: 'sebuah thread',
+      body: 'Lorem ipsum dolor sit amet',
+      owner: 'user-123',
+    };
+    const expectedAddedThread = new AddedThread({
+      id: 'thread-123',
+      title: useCasePayload.title,
+      body: useCasePayload.body,
+      owner: useCasePayload.owner,
+    });
+
+    /** creating dependency of use case */
+    const mockThreadRepository = new ThreadRepository();
+
+    /** mocking needed function */
+    mockThreadRepository.addThread = jest.fn()
+      .mockImplementation(() => Promise.resolve(expectedAddedThread));
+
+    /** creating use case instance */
+    const getThreadUseCase = new AddThreadUseCase({
+      threadRepository: mockThreadRepository,
+    });
+
+    // Action
+    const addedThread = await getThreadUseCase.execute(useCasePayload);
+
+    // Assert
+    expect(addedThread).toStrictEqual(expectedAddedThread);
+    expect(mockThreadRepository.addThread).toBeCalledWith(new AddThread({
+      title: useCasePayload.title,
+      body: useCasePayload.body,
+      owner: useCasePayload.owner,
+    }));
+  });
+});
---
+++ src/Domains/threads/entities/_test/AddedThread.test.js
@@ -0,0 +1,40 @@
+const AddedThread = require('../AddedThread');
+
+describe('a AddedThread entities', () => {
+  it('should throw error when payload did not contain needed property', () => {
+    const payload = {
+      title: 'sebuah thread',
+      body: 'Lorem ipsum dolor sit amet',
+    };
+
+    expect(() => new AddedThread(payload)).toThrowError('ADDED_THREAD.NOT_CONTAIN_NEEDED_PROPERTY');
+  });
+
+  it('should throw error when payload did not meet data type specification', () => {
+    const payload = {
+      id: 123,
+      title: 'sebuah thread',
+      owner: true,
+    };
+
+    expect(() => new AddedThread(payload)).toThrowError('ADDED_THREAD.NOT_MEET_DATA_TYPE_SPECIFICATION');
+  });
+
+  it('should create new thread object correctly', () => {
+    // Arrange
+    const payload = {
+      id: 'thread-123',
+      title: 'sebuah thread',
+      owner: 'user-123',
+    };
+
+    // Action
+    const addedThread = new AddedThread(payload);
+
+    // Assert
+    expect(addedThread.id).toEqual(payload.id);
+    expect(addedThread.title).toEqual(payload.title);
+    expect(addedThread.body).toEqual(payload.body);
+    expect(addedThread.owner).toEqual(payload.owner);
+  });
+});
